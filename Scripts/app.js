9133968d5eb410faa395ad9035ac7fb8bdd9c9b7
Joseph Stone <josephdstone@hotmail.com>
2014-10-13 08:18:44 -0600

    refactor

---
+++ Scripts/app.js
@@ -0,0 +1,35 @@
+﻿var wrightApp = angular.module("wrightApp",
+    ['ngRoute']);
+wrightApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/',
+            {
+                templateUrl: '/Views/partials/Home.html'
+            })
+        .when('/Realtors',
+            {
+                templateUrl: '/Views/partials/Realtors.html'
+            })
+        .when('/About',
+            {
+                templateUrl: '/Views/partials/About.html'
+            })
+        .when('/employeeAccount',
+            {
+                templateUrl: '/Views/partials/employeeAccount.html'
+            })
+        .when('/batches',
+            {
+                templateUrl: '/Views/partials/batches.html'
+            })
+        .when('/packages',
+            {
+                templateUrl: '/Views/partials/packages.html'
+            })
+        .when('/search',
+            {
+                templateUrl: '/Views/partials/search.html'
+            })
+        .otherwise({ redirectTo: '/' });
+
+});
