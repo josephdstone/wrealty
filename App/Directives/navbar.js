a4a92f20bafde05717b3973a573cbfb200be6a88
josephdstone <josephdstone@hotmail.com>
2014-10-13 23:00:14 -0400

    navbar

---
+++ App/Directives/navbar.js
@@ -0,0 +1,28 @@
+﻿
+(function() {
+	'use strict';
+	angular.module('wrightApp').directive('navBar', NavBar);
+
+	function NavBar() {
+		return {
+			restrict: 'E',
+			replace: true,
+			scope: {},
+			templateUrl: 'App/Directives/navBar.html',
+			controller: 'NavBarCtrl',
+			controllerAs: 'vm'
+		}
+	}
+})();
+
+(function() {
+	'use strict';
+	angular.module('wrightApp')
+		.controller('NavBarCtrl', NavBarCtrl);
+
+	function NavBarCtrl() {
+		var vm = this;
+
+
+	}
+})();
